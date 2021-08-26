ac79d203a0680634aeb5e9b78705c60e0d418774
CCMDLR98 <43662095+CCMDLR98@users.noreply.github.com>
2021-08-26 14:05:51 -0600

    'leetcode2'

--- LeetCode/A-P1.js
+++ LeetCode/A-P1.js
@@ -3,16 +3,33 @@
  * @param {number} target
  * @return {number[]}
  */
- const twoSum = function (nums, target) {
+const twoSum = function (nums, target) {
+   //se crea el HASH
     const hash = {};
-
-    for (let param = 0;param<nums.length;param++) {
-        const x = nums[param];
-        if (hash[target - x] !== undefined) {
-            return [hash[target - x], param];
+    // se realiza un loop de los nums recibidos
+    for (let param = 0; param < nums.length; param++) {
+        const x = nums[param];//tomamos el valor actual de nums
+        if (hash[target - x] !== undefined) { // evaluamos si el numero que necesitamos ADD al valor acutal {x} para llegar a nuestro objetivo {target} ha sido visto
+            return [hash[target - x], param];//tomamos el index del numero visto y el index del valor actual.
         }
-        hash[x]=param;
+        hash[x] = param;//actualizamos el el hash para incluir el valor que recien se evaluo junto con su index.
     }
-    return [];
+    return []; // si no se agregan numeros que sumados den el target se  regresa un array vacio.
 };
-twoSum([2, 3, 4, 5], 9);
+twoSum('twoSum: ',[2, 3, 4, 5], 9);
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+ var addTwoNumbers = function(l1, l2) {
+        console.log(l1.reverse());
+};
